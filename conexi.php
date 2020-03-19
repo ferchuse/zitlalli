793b852714551edd58a93d474a64fa020d96a069
ferchuse <sistemas@glifo.mx>
2020-03-19 12:38:27 -0600

    Revert "Update conexi.php"
    This reverts commit 99ea0fde5455eea5a107d6340f5e1b5ba9de7f50.

--- conexi.php
+++ conexi.php
@@ -36,12 +36,12 @@
 		}
 		
 		
-		// if($_SERVER["SERVER_NAME"] != "localhost") {
-			// mysqli_query($link, "SET NAMES 'utf8'") or die("Error Cambiando charset").mysqli_error($link);
+		if($_SERVER["SERVER_NAME"] != "localhost") {
+			mysqli_query($link, "SET NAMES 'utf8'") or die("Error Cambiando charset").mysqli_error($link);
 			
 			
-			// mysqli_query($link, "SET CHARACTER SET utf8") or die("Error en charset UTF8".mysqli_error($link));
-		// }
+			mysqli_query($link, "SET CHARACTER SET utf8") or die("Error en charset UTF8".mysqli_error($link));
+		}
 		
 		
 		return $link;
