2cc1ffff753add0e32d0a678b0724d4ac67fdaf2
ferchuse <sistemas@glifo.mx>
2020-04-10 14:34:02 -0500

    Busca en tarjetas no canceladas

--- paginas/recaudacion/control/buscar_unidad.php
+++ paginas/recaudacion/control/buscar_unidad.php
@@ -1,8 +1,6 @@
 <?php 
 	
 	include('../../../conexi.php');
-	include('../../../funciones/generar_select.php');
-	// include('../../../funciones/console_log.php');
 	$link = Conectarse();
 	$filas = array();
 	$respuesta = array();
@@ -41,7 +39,8 @@
 	LEFT JOIN tarjetas USING (id_unidades)
 	WHERE
 	num_eco = '{$_GET['num_eco']}'
-	
+	AND estatus_tarjetas <> 'Cancelado' 
+	ORDER BY fecha_tarjetas DESC LIMIT 1 
 	";
   
 	$respuesta["consulta"] = "$consulta";
