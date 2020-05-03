09dc034a2d3717379dd3a4dc30614c762960df81
ferchuse <sistemas@glifo.mx>
2020-05-03 12:53:01 -0500

    Movil

--- paginas/recaudacion/abono_general.php
+++ paginas/recaudacion/abono_general.php
@@ -36,8 +36,8 @@
 					<!--Form Filtro !-->
 					<form id="form_filtro" autocomplete="off">
 						<div class="row mb-2">
-							<div class="col-12">
-								<div class="col-12 mb-3">
+							<div class="col-sm-12">
+								<div class="col-sm-12 mb-3">
 									<button  class="btn btn-primary btn-sm" >
 										<i class="fas fa-search"></i> Buscar
 									</button>
@@ -54,36 +54,36 @@
 						</div>
 						
 						<div class="row mb-2">
-							<div class="col-1">
+							<div class="col-sm-1">
 								<label for="nombre_condonaciones">Fecha Inicial:</label>
 							</div>
-							<div class="col-2">			
+							<div class="col-sm-2">			
 								<input class="form-control" type="date" name="fecha_inicial" id="fecha_inicial" value="<?php echo date("Y-m-d");?>">
 							</div>  
-							<div class="col-1">
+							<div class="col-sm-1">
 								<label for="nombre_condonaciones">Fecha Final:</label>
 							</div>	
-							<div class="col-2">			
+							<div class="col-sm-2">			
 								<input class="form-control" type="date" name="fecha_final" id="fecha_final" value="<?php echo date("Y-m-d");?>">
 							</div> 
 						</div>
 						<div class="row mb-2"> 
-							<div class="col-1">
+							<div class="col-sm-1">
 								<label for="nombre_condonaciones">No. Economico:</label>
 							</div>	
-							<div class="col-2">			
+							<div class="col-sm-2">			
 								<input class="form-control" type="text" name="num_eco" id="num_eco" >
 							</div> 
-							<div class="col-1">
+							<div class="col-sm-1">
 								<label >Usuario:</label>
 							</div>	
-							<div class="col-2">			
+							<div class="col-sm-2">			
 								<?php echo generar_select($link, "usuarios", "id_usuarios", "nombre_usuarios", true, false, false, $_SESSION["id_usuarios"])?>
 							</div> 
-							<div class="col-1">
+							<div class="col-sm-1">
 								<label >Empresa:</label>
 							</div>	
-							<div class="col-2">			
+							<div class="col-sm-2">			
 								<?php echo generar_select($link, "empresas", "id_empresas", "nombre_empresas", true)?>
 							</div> 
 						</div>
--- paginas/recaudacion/abonos_unidades.php
+++ paginas/recaudacion/abonos_unidades.php
@@ -51,8 +51,8 @@
 					<!--Form Filtro !-->
 					<form id="form_filtro" autocomplete="off">
 						<div class="row mb-2">
-							<div class="col-12">
-								<div class="col-12 mb-3">
+							<div class="col-sm-12">
+								<div class="col-sm-12 mb-3">
 									<button class="btn btn-primary btn-sm" >
 										<i class="fas fa-search"></i> Buscar
 									</button>
@@ -66,46 +66,46 @@
 						</div>
 						
 						<div class="row mb-2">
-							<div class="col-1">
+							<div class="col-sm-1">
 								<label for="nombre_condonaciones">Fecha Inicial:</label>
 							</div>
-							<div class="col-2">			
+							<div class="col-sm-2">			
 								<input class="form-control" type="date" name="fecha_inicial" id="fecha_inicial" value="<?php echo date("Y-m-d");?>">
 							</div>  
-							<div class="col-1">
+							<div class="col-sm-1">
 								<label for="nombre_condonaciones">Fecha Final:</label>
 							</div>	
-							<div class="col-2">			
+							<div class="col-sm-2">			
 								<input class="form-control" type="date" name="fecha_final" id="fecha_final" value="<?php echo date("Y-m-d");?>">
 							</div> 
-							<div class="col-1">
+							<div class="col-sm-1">
 								<label >Usuario:</label>
 							</div>	
-							<div class="col-2">			
+							<div class="col-sm-2">			
 								<?php echo generar_select($link, "usuarios", "id_usuarios", "nombre_usuarios", true, false, false, $_SESSION["id_usuarios"])?>
 							</div> 
 						</div>
 						<div class="row mb-2"> 
-							<div class="col-1">
+							<div class="col-sm-1">
 								<label for="nombre_condonaciones">No. Economico:</label>
 							</div>	
-							<div class="col-2">			
+							<div class="col-sm-2">			
 								<input class="form-control" type="text" name="num_eco" id="num_eco" >
 							</div>  	
-							<div class="col-1">
+							<div class="col-sm-1">
 								<label for="nombre_condonaciones">Estatus:</label>
 							</div>	
-							<div class="col-2">			
+							<div class="col-sm-2">			
 								<select name="estatus_abonos" class="form-control">
 									<option>Todos</option>
 									<option>Activo</option>
 									<option>Cancelado</option>
 								</select>
 							</div>  
-							<div class="col-1">
+							<div class="col-sm-1">
 								<label >Empresa:</label>
 							</div>	
-							<div class="col-2">			
+							<div class="col-sm-2">			
 								<?php echo generar_select($link, "empresas", "id_empresas", "nombre_empresas", true)?>
 							</div> 
 						</div>
