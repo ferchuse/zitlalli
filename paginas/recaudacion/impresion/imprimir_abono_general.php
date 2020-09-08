4e89cf2995e63f7f063ce6430d5d9ba7f814d414
ferchuse <sistemas@glifo.mx>
2020-09-08 09:06:02 -0500

    Update imprimir_abono_general.php

--- paginas/recaudacion/impresion/imprimir_abono_general.php
+++ paginas/recaudacion/impresion/imprimir_abono_general.php
@@ -48,6 +48,22 @@
 		
 		$print.="\n\nVB";
 		
+		
+		$print.= "@";
+		$print.= "!".chr(16)."ABONO GENERAL"."!".chr(0)."\n";
+		$print.= "Folio: ".$filas["id_abonogeneral"]."\n";
+		$print.= "Fecha: ".$filas["fecha_abonogeneral"]."\n";
+		$print.= "Usuario: ".$filas["nombre_usuarios"]."\n";
+		$print.= "Num Eco: ".$filas["num_eco"]."\n";
+		$print.= "Motivo: ".$filas["nombre_motivosAbono"]."\n";
+		$print.= "Depositante: ".$filas["depositante"]."\n";
+		$print.= "Concepto: $".$filas["concepto_abonogeneral"]."\n ";
+		$print.= "Bueno Por: $".$filas["monto_abonogeneral"]."\n";
+		$print.= NumeroALetras::convertir($filas["monto_abonogeneral"], 'PESOS', 'CENTAVOS')."\n";
+		
+		
+		$print.="\n\nVB";
+		
 		echo base64_encode($print);
 		
 		
