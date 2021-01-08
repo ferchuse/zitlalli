74788ba1c0ac900c054f35b69a09fbdec1b6f1ed
ferchuse <sistemas@glifo.mx>
2021-01-08 12:16:47 -0600

    Cargos 2021

--- paginas/recaudacion/cargos.php
+++ paginas/recaudacion/cargos.php
@@ -66,17 +66,34 @@
 								<label for="nombre_condonaciones">Mes:</label>
 							</div>
 							<div class="col-2">			
+						
+							
 								<select class="form-control" name="mes_cargos">
-									<option <?= date("Ym") == "202012" ? "selected" : ""?> value="202012">2020-12 </option>
-									<option <?= date("Ym") == "202011" ? "selected" : ""?> value="202011">2020-11 </option>
-									<option <?= date("Ym") == "202010" ? "selected" : ""?> value="202010">2020-10 </option>
-									<option <?= date("Ym") == "202009" ? "selected" : ""?> value="202009">2020-09 </option>
-									<option <?= date("Ym") == "202008" ? "selected" : ""?> value="202008">2020-08 </option>
-									<option <?= date("Ym") == "202007" ? "selected" : ""?> value="202007">2020-07 </option>
-									<option <?= date("Ym") == "202006" ? "selected" : ""?> value="202006">2020-06 </option>
-									<option <?= date("Ym") == "202005" ? "selected" : ""?> value="202005">2020-05 </option>
-									<option <?= date("Ym") == "202004" ? "selected" : ""?> value="202004">2020-04 </option>
-									
+									<option value="202112">2021-12 </option>
+									<option value="202111">2021-11 </option>
+									<option value="202110">2021-10 </option>
+									<option value="202109">2021-09 </option>
+									<option value="202108">2021-08 </option>
+									<option value="202107">2021-07 </option>
+									<option value="202106">2021-06 </option>
+									<option value="202105">2021-05 </option>
+									<option value="202104">2021-04 </option>
+									<option value="202103">2021-03 </option>
+									<option value="202102">2021-02 </option>
+									<option selected value="202101">2021-01 </option>
+									<option value="202012">2020-12 </option>
+									<option value="202011">2020-11 </option>
+									<option value="202010">2020-10 </option>
+									<option value="202009">2020-09 </option>
+									<option value="202008">2020-08 </option>
+									<option value="202007">2020-07 </option>
+									<option value="202006">2020-06 </option>
+									<option value="202005">2020-05 </option>
+									<option value="202004">2020-04 </option>
+									<option value="202003">2020-03 </option>
+									<option value="202002">2020-02 </option>
+									<option value="202001">2020-01 </option>
+							
 								</select>
 							</div> 
 							<div class="col-1">
