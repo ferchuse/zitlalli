0bda61f051f319b2201ed4e3acd2067dda612eff
ferchuse <sistemas@glifo.mx>
2021-02-22 09:55:36 -0600

    Update conexi.php

--- conexi.php
+++ conexi.php
@@ -25,7 +25,7 @@
 		$set_names = "SET NAMES 'utf8'";
 		date_default_timezone_set('America/Mexico_City');
 		
-    if (!($link=mysqli_connect($host,$usuario,$pass)))
+		if (!($link=mysqli_connect($host,$usuario,$pass)))
 		{
 			die( "Error conectando a la base de datos.". mysqli_error($link));
 		}
@@ -37,12 +37,13 @@
 		
 		
 		if($_SERVER["SERVER_NAME"] != "localhost") {
-		
+			
 			mysqli_query($link, "SET sql_mode = ''") or die("Error Cambiando sqlmode").mysqli_error($link);
 			// mysqli_query($link, "SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));") or die("Error Cambiando sqlmode").mysqli_error($link);
 			
 			
 			mysqli_query($link, "SET CHARACTER SET utf8") or die("Error en charset UTF8".mysqli_error($link));
+			mysqli_query($link, "SET session max_execution_time=300000") or die("Error enmax execution".mysqli_error($link));
 		}
 		
 		
